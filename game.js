90489b20db92e22d9341f20973229f9dec45f6ce
rastringer <ra1stringer@gmail.com>
2015-09-30 16:18:35 -0700

    updates with score, msg

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 
-var count = 0
+var count = 0;
+var name = prompt("What's your name?");
 var question1 = prompt("Did I grow up in Hawaii?").toUpperCase();
 
 
-if (question1 === 'yes' ||'y') {
+if (question1 === 'YES' || question1 === 'Y') {
 	alert("Nope, I grew up in the U.K.");
 }
 else {
@@ -14,7 +15,7 @@ else {
 
 var question2 = prompt("Do I speak Mandarin?").toUpperCase();
 
-if (question2 === 'yes' || 'y') {
+if (question2 === 'YES' || question2 === 'Y') {
 
 	alert("I wish! Unfortunately I don't speak Mandarin.")
 }
@@ -26,7 +27,7 @@ else {
 
 var question3 = prompt("Do I own a motorcycle?").toUpperCase();
 
-if (question3 === 'yes' || 'y') {
+if (question3 === 'YES' || question3 ===  'Y') {
 	alert("Incorrect. I do have a trusty mountain bike however.")
 }
 else {
@@ -34,12 +35,18 @@ else {
 	count++;
 }
 
-//comments rock
 
 console.log('Question 1 repsonse: ' + question1);
 
 console.log('Question 2 response: ' + question2);
 
-
 console.log('Question 3 response: ' + question3);
+
+alert("You answered '" + question1 + "' to whether I grew up in Hawaii.");
+
+alert("You answered '" + question2 + "' to whether I speak Mandarin.");
+
+alert("You answered '" + question3 + "' to whether I own a motorcycle.");
+
+alert("Congratulations, " + name + ", you answered " + count + " questions out of 3 correctly.");
 
