cf3a6c033d69379a1657bf76ebeccfc576d699b3
Sabrina Tee <sabrinatee@me.com>
2015-10-01 13:57:17 -0700

    Moving alerts to DOM

--- game.js
+++ game.js
@@ -1,56 +1,52 @@
+document.getElementById('one');
+document.getElementById('two');
+document.getElementById('three');
 
 var count = 0;
 var name = prompt("What's your name?");
+alert("Welcome " + name + "!");
 
 function ques1() {
   var question1 = prompt("Did I grow up in Hawaii?").toUpperCase();
+  console.log('Question 1 repsonse: ' + question1);
   if (question1 === 'YES' || question1 === 'Y') {
-    alert("Nope, I grew up in the U.K.");
+    one.innerHTML = "Nope, I grew up in the U.K.";
   }
   else {
-    alert("Correct!");
+    one.innerHTML = "Correct!";
     count++;
-  }
+  };
 }
 
 function ques2() {
   var question2 = prompt("Do I speak Mandarin?").toUpperCase();
+  console.log('Question 2 response: ' + question2);
   if (question2 === 'YES' || question2 === 'Y') {
-    alert("I wish! Unfortunately I don't speak Mandarin.")
+    two.innerHTML = "I wish! Unfortunately I don't speak Mandarin."
   }
   else {
-	 alert("Correct");
+	 two.innerHTML ="Correct";
 	 count++;
-  }
+  };
 }
 
 function ques3() {
   var question3 = prompt("Do I own a motorcycle?").toUpperCase();
+  console.log('Question 3 response: ' + question3);
   if (question3 === 'YES' || question3 ===  'Y') {
-	 alert("Incorrect. I do have a trusty mountain bike however.")
+	 three.innerHTML = "Incorrect. I do have a trusty mountain bike however."
   }
   else {
-	 alert("Correct");
+	 three.innerHTML = "Correct";
 	 count++;
-  }
+  };
 }
 
-
-console.log('Question 1 repsonse: ' + question1);
-
-console.log('Question 2 response: ' + question2);
-
-console.log('Question 3 response: ' + question3);
-
 ques1();
+
 ques2();
+
 ques3();
-
-alert("You answered '" + question1 + "' to whether I grew up in Hawaii.");
-
-alert("You answered '" + question2 + "' to whether I speak Mandarin.");
-
-alert("You answered '" + question3 + "' to whether I own a motorcycle.");
 
 alert("Congratulations, " + name + ", you answered " + count + " questions out of 3 correctly.");
 
