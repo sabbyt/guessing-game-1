65201ee55facd9e4954d4184c917740257be2cbd
Sabrina Tee <sabrinatee@me.com>
2015-10-01 13:33:19 -0700

    Adding functions

--- game.js
+++ game.js
@@ -1,38 +1,38 @@
 
 var count = 0;
 var name = prompt("What's your name?");
-var question1 = prompt("Did I grow up in Hawaii?").toUpperCase();
 
-
-if (question1 === 'YES' || question1 === 'Y') {
-	alert("Nope, I grew up in the U.K.");
-}
-else {
-	alert("Correct!");
-	count++;
-}
-
-
-var question2 = prompt("Do I speak Mandarin?").toUpperCase();
-
-if (question2 === 'YES' || question2 === 'Y') {
-
-	alert("I wish! Unfortunately I don't speak Mandarin.")
-}
-else {
-	alert("Correct");
-	count++;
+function ques1() {
+  var question1 = prompt("Did I grow up in Hawaii?").toUpperCase();
+  if (question1 === 'YES' || question1 === 'Y') {
+    alert("Nope, I grew up in the U.K.");
+  }
+  else {
+    alert("Correct!");
+    count++;
+  }
 }
 
-
-var question3 = prompt("Do I own a motorcycle?").toUpperCase();
-
-if (question3 === 'YES' || question3 ===  'Y') {
-	alert("Incorrect. I do have a trusty mountain bike however.")
+function ques2() {
+  var question2 = prompt("Do I speak Mandarin?").toUpperCase();
+  if (question2 === 'YES' || question2 === 'Y') {
+    alert("I wish! Unfortunately I don't speak Mandarin.")
+  }
+  else {
+	 alert("Correct");
+	 count++;
+  }
 }
-else {
-	alert("Correct");
-	count++;
+
+function ques3() {
+  var question3 = prompt("Do I own a motorcycle?").toUpperCase();
+  if (question3 === 'YES' || question3 ===  'Y') {
+	 alert("Incorrect. I do have a trusty mountain bike however.")
+  }
+  else {
+	 alert("Correct");
+	 count++;
+  }
 }
 
 
@@ -41,6 +41,10 @@ console.log('Question 1 repsonse: ' + question1);
 console.log('Question 2 response: ' + question2);
 
 console.log('Question 3 response: ' + question3);
+
+ques1();
+ques2();
+ques3();
 
 alert("You answered '" + question1 + "' to whether I grew up in Hawaii.");
 
